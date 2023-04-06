5d7178dd97d07d5c28e88f5e8f8ec7b3f758b03b
Mark Owings <owingsmk@mail.uc.edu>
2023-04-06 17:50:36 -0400

    11th commit and push to update github

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -54,69 +54,75 @@ public class SafeInput {
 
         return retVal;
     }
-    public static int getRangedInt(Scanner pipe, String prompt, int low, int high){ //this is setting the range variable
-
-        int retVal = 0;
-        String trash = "";
-        boolean done = false;
-
-        do{
-            System.out.println(prompt);
-            if(pipe.hasNextInt()){
-                retVal = pipe.nextInt();
-                pipe.nextInt();
-                if (retVal >= low && retVal <= high)
-                done = true;
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high) {
+        boolean hasGoodInput = false;
+        int input = 0;
+        do {
+            System.out.print("%s between %d and %d: ", prompt, low, high);
+            if(pipe.hasNextInt()) {
+                input = pipe.nextInt();
+                if(input >= low && input <= high) {
+                    hasGoodInput = true;
+                }
+                else {
+                    if(input < low) {
+                        System.out.println("The number you entered was below the acceptable range. Please try again");
+                    }
+                    else {
+                        System.out.println("The number you entered was above the acceptable range. Please try again");
+                    }
+                }
             }
-            else{
-                trash = pipe.nextLine();
-                System.out.println("Not correct type: "+ trash);
+            else {
+                System.out.println("You did not enter an integer. Please try again.");
             }
-        }while(!done);
-
-        return retVal;
+            pipe.nextLine();
+        } while(!hasGoodInput);
+        return input;
     }
-    public static int getRangedDouble(Scanner pipe, String prompt, double low, double high){ //this is setting the range variable
 
-        double retVal = 0;
-        String trash = "";
-        boolean done = false;
-
-        do{
-            System.out.println(prompt);
-            if(pipe.hasNextDouble()){
-                retVal = pipe.nextDouble();
-                pipe.nextLine();
-                done = true;
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high) {
+        boolean hasGoodInput = false;
+        double input = 0;
+        do {
+            System.out.print(prompt +" between " + low + " and " + high + ": ");
+            if(pipe.hasNextDouble()) {
+                input = pipe.nextDouble();
+                if(input >= low && input <= high) {
+                    hasGoodInput = true;
+                }
+                else {
+                    if(input < low) {
+                        System.out.println("The number you entered was below the acceptable range. Please try again");
+                    }
+                    else {
+                        System.out.println("The number you entered was above the acceptable range. Please try again");
+                    }
+                }
             }
-            else{
-                trash = pipe.nextLine();
-                System.out.println("Not correct type: "+ trash); //shows what was typed as tells them it is incorrect
+            else {
+                System.out.println("You did not enter an double. Please try again.");
             }
-        }while(!done);
-
-        return (int) retVal;
+            pipe.nextLine();
+        } while(!hasGoodInput);
+        return input;
     }
-    public static boolean getYNConfirm(Scanner pipe, String prompt){ //this is for Yes or No variable
 
-        String retVal = "y/n";
-        String trash = "";
-        boolean done = false;
+    public static boolean getYNComfirm(Scanner pipe, String prompt) {
 
-        do{
-            System.out.println(prompt);
-            if(pipe.hasNextBoolean()){
-                retVal = String.valueOf(pipe.nextBoolean());
-                pipe.nextLine();
-                done = true;
-            }
-            else{
-                trash = pipe.nextLine();
-                System.out.println("Not correct type: "+ trash);
-            }
-        }while(!done);
+        boolean confirm = false;
 
-        return retVal.isEmpty();
+        char input;
+
+        System.out.print(prompt + " (Y/N): ");
+
+        input = pipe.nextLine().toLowerCase().charAt(0);
+
+        if(input == 'y') {
+
+            confirm = true;
+        }
+        return confirm;
     }
 
     public static String getRegExString(Scanner pipe, String prompt, String regExPattern){
